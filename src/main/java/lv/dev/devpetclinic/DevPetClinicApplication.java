f07965dd3b7be84f22d4420aa58a7e372b5d1f9e
kkemers <kkemers@inbox.lv>
2021-01-26 20:26:32 +0200

    Init project

---
+++ src/main/java/lv/dev/devpetclinic/DevPetClinicApplication.java
@@ -0,0 +1,13 @@
+package lv.dev.devpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/dev/devpetclinic/DevPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.dev.devpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
