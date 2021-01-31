63be7ea31128280ca581f08c76238cb88b8f20c2
kkemers <kkemers@inbox.lv>
2021-01-31 18:17:59 +0200

    Add base entity. Close #10.

---
+++ pet-clinic-data/src/main/java/pet_clinic_data/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package pet_clinic_data.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/pet_clinic_data/model/Pet.java
+++ pet-clinic-data/src/main/java/pet_clinic_data/model/Pet.java
@@ -2,7 +2,7 @@ package pet_clinic_data.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private String name;
     private PetType petType;
