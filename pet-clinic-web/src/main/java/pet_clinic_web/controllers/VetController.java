fa36b55195d830bc834630b7a9b7b0d821e9655d
kkemers <kkemers@inbox.lv>
2021-03-07 09:03:06 +0200

    Add vet page and controller. Closes #7

---
+++ pet-clinic-web/src/main/java/pet_clinic_web/controllers/VetController.java
@@ -0,0 +1,14 @@
+package pet_clinic_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";
+    }
+}
