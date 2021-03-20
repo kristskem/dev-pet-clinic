9dc66ae8eb47acb4e1633f8965a7cb1aa4faac69
kkemers <kkemers@inbox.lv>
2021-03-20 16:09:17 +0200

    Add specialty entity. Closes #27

--- pet-clinic-data/src/main/java/lv/dev/pet_clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/lv/dev/pet_clinic/model/Vet.java
@@ -1,10 +1,16 @@
 package lv.dev.pet_clinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
-    public Vet() {}
-
-    public Vet(String firstName, String lastName) {
-        super(firstName, lastName);
+    public Set<Specialty> getSpecialtySet() {
+        return specialtySet;
     }
+
+    public void setSpecialtySet(Set<Specialty> specialtySet) {
+        this.specialtySet = specialtySet;
+    }
+
+    private Set<Specialty> specialtySet;
 }
