b1c15b16fe20644f9c368f5c18b350a26621486b
kkemers <kkemers@inbox.lv>
2021-03-20 13:27:15 +0200

    Add layout to index page. Closes #19

--- pet-clinic-web/src/main/java/lv/dev/pet_clinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/lv/dev/pet_clinic/controllers/OwnerController.java
@@ -15,7 +15,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/index", "/index.html"})
+    @RequestMapping({"", "/index", "/templates/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
