3354beb0ae8ec7038ae73f47aa1d0d1c1abcc272
kkemers <kkemers@inbox.lv>
2021-03-20 16:04:30 +0200

    Add visit entity. Closes #26

---
+++ pet-clinic-data/src/main/java/lv/dev/pet_clinic/model/Visit.java
@@ -0,0 +1,37 @@
+package lv.dev.pet_clinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    private String description;
+    private Pet pet;
+
+
+}
