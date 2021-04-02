0b8d18e489c3334d064f014ce408d8b6e0dd3752
kkemers <kkemers@inbox.lv>
2021-04-02 09:01:04 +0300

    Implement Jpa entities.

--- pet-clinic-data/src/main/java/lv/dev/pet_clinic/model/Person.java
+++ pet-clinic-data/src/main/java/lv/dev/pet_clinic/model/Person.java
@@ -1,8 +1,15 @@
 package lv.dev.pet_clinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "fast_name")
     private String lastName;
 
     public Person() {
--- pet-clinic-data/src/main/java/lv/dev/pet_clinic/model/Visit.java
+++ pet-clinic-data/src/main/java/lv/dev/pet_clinic/model/Visit.java
@@ -1,10 +1,22 @@
 package lv.dev.pet_clinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "pet")
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
 
     public LocalDate getDate() {
         return date;
@@ -29,9 +41,5 @@ public class Visit extends BaseEntity {
     public void setPet(Pet pet) {
         this.pet = pet;
     }
-
-    private String description;
-    private Pet pet;
-
 
 }
