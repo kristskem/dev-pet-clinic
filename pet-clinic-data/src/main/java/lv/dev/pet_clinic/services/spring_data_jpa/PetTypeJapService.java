95d6b734d58712025a1f91f6c221a8708a22131e
kkemers <kkemers@inbox.lv>
2021-04-02 10:51:24 +0300

    Implement jpa services

---
+++ pet-clinic-data/src/main/java/lv/dev/pet_clinic/services/spring_data_jpa/PetTypeJapService.java
@@ -0,0 +1,48 @@
+package lv.dev.pet_clinic.services.spring_data_jpa;
+
+import lv.dev.pet_clinic.model.PetType;
+import lv.dev.pet_clinic.repositories.PetTypeRepository;
+import lv.dev.pet_clinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJpa")
+public class PetTypeJapService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeJapService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/lv/dev/pet_clinic/services/spring_data_jpa/VetJpaService.java
@@ -0,0 +1,48 @@
+package lv.dev.pet_clinic.services.spring_data_jpa;
+
+import lv.dev.pet_clinic.model.Vet;
+import lv.dev.pet_clinic.repositories.VetRepository;
+import lv.dev.pet_clinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJpa")
+public class VetJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
