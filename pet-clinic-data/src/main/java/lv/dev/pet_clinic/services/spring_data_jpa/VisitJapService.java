c32a1a959421d77677af2fefce5827df146a6386
kkemers <kkemers@inbox.lv>
2021-04-02 11:22:47 +0300

    Implement visit jpa service

---
+++ pet-clinic-data/src/main/java/lv/dev/pet_clinic/services/spring_data_jpa/VisitJapService.java
@@ -0,0 +1,48 @@
+package lv.dev.pet_clinic.services.spring_data_jpa;
+
+import lv.dev.pet_clinic.model.Visit;
+import lv.dev.pet_clinic.repositories.VisitRepository;
+import lv.dev.pet_clinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJpa")
+public class VisitJapService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJapService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
